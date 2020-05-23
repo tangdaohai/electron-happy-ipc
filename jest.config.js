c5d3a0bba7ca522522adde09adcb11b912bcfdf4
唐道海 <tangdaohai@outlook.com>
2020-05-24 00:25:40 +0800

    build(test): add jest coverage

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+const {defaults} = require('jest-config')
+module.exports = {
+  coveragePathIgnorePatterns: [
+    ...defaults.coveragePathIgnorePatterns,
+    'test-driver',
+    'src/request/index.ts',
+    'src/server/index.ts'
+  ],
+  coverageReporters: ['text']
+}
