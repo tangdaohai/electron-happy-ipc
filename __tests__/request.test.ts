d7065c1a9a422377045cb943d18fb4e663ae5ff0
唐道海 <tangdaohai@outlook.com>
2020-05-26 23:00:30 +0800

    test(test): add global config test

--- __tests__/request.test.ts
+++ __tests__/request.test.ts
@@ -1,14 +1,14 @@
 
 import { ipcMain, ipcRenderer } from '../test-driver'
 import serverService from '../src/server/server'
-import requestService, { IPCRequestOptions } from '../src/request/request'
+import requestService, { setConfig, IPCRequestOptions } from '../src/request/request'
+
+// @ts-ignore
+const server = serverService(ipcMain)
+// @ts-ignore
+const request = requestService(ipcRenderer)
 
 describe('use ipc request', () => {
-  
-  // @ts-ignore
-  const server = serverService(ipcMain)
-  // @ts-ignore
-  const request = requestService(ipcRenderer)
 
   test('normal request', async () => {
     const type = 'normal request'
@@ -22,25 +22,28 @@ describe('use ipc request', () => {
     expect(result).toBe(value.foo)
   })
 
-  test('replace', async () => {
+  test('replace',(done) => {
     const options: IPCRequestOptions = {
       type: 'request test',
       replace: true
     }
     let i = 1
     server.use(options.type, (ctx, data) => {
-      setTimeout(() => {
-        i++
-        ctx.reply(i)
-      }, 0)
+      i++
+      ctx.reply(i)
     })
 
     // 此次请求结果应该是 2，如果此次请求没有被替换的话
     request(options).catch(err => {
       expect(err.code).toMatch('replace')
     })
-    const result = await request(options)
-    expect(result).toBe(2)
+    
+    // @FIXME 因为 request symbol 生成的机制，同步运行 可能会产生相同的 symbol
+    setTimeout(async () => {
+      const result = await request(options)
+      expect(result).toBe(3)
+      done()
+    }, 0)
   })
 
   test('timeout', async () => {
@@ -60,5 +63,39 @@ describe('use ipc request', () => {
     } catch (err) {
       expect(err.code).toMatch('timeout')
     }
+  })
+})
+
+describe ('use ipc request with config', () => {
+
+  beforeEach(() => {
+    setConfig({
+      replace: true,
+      timeout: 500
+    })
+  })
+
+  test('global config: replace is true', async (done) => {
+    const type = 'global config: replace'
+    server.use(type, (ctx, data) => {
+      setTimeout(() => {
+        ctx.reply(data)
+      }, 50)
+    })
+
+    request(type, 1).catch(err => {
+      expect(err.code).toMatch('replace')
+    })
+
+    setTimeout(async () => {
+      const result = await request(type, 2)
+      expect(result).toBe(2)
+      done()
+    }, 0)
+  })
+
+  afterEach(() => {
+    // clean
+    setConfig()
   })
 })
